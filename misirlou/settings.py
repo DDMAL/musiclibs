a78be441e3b1bade520a8cbbe58184394ad9db57
William Bain <bain.william.a@gmail.com>
2015-09-11 14:02:09 -0400

    New: Add core jspm setup

--- misirlou/settings.py
+++ misirlou/settings.py
@@ -117,3 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    ('js', os.path.join(BASE_DIR, 'misirlou/frontend/js')),
+)
