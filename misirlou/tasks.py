79d0f9f5407287cb1c87466dc4468d0966a83395
Alex Parmentier <a.g.parmentier@gmail.com>
2015-09-17 19:35:55 -0400

    WIP: Existing celery tasks get status SENT.
    This listener will set all new tasks status's to SENT. This will allow us to distinguish between non-existing tasks (which return status=PENDING) and tasks which are in queue but have not yet begun processing (which ALSO normally return status=PENDING).

--- misirlou/tasks.py
+++ misirlou/tasks.py
@@ -1,11 +1,22 @@
 from __future__ import absolute_import
 
 from celery import shared_task
+from celery import current_app
+from celery.signals import after_task_publish
 from .models.manifest import Manifest
-
 import uuid
 
 @shared_task
 def create_manifest(remote_url, shared_id):
     manifest = Manifest(remote_url=remote_url, uuid=uuid.UUID(shared_id))
     manifest.save()
+
+@after_task_publish.connect
+def update_sent_state(sender=None, body=None, **kwargs):
+    # Change task.status to 'SENT' for all tasks which are sent in.
+    # This allows one to distinguish between PENDING tasks which have been
+    # sent in and tasks which do not exist. State will change to
+    # SUCCESS, FAILURE, etc. once the process terminates.
+    task = current_app.tasks.get(sender)
+    backend = task.backend if task else current_app.backend
+    backend.store_result(body['id'], None, "SENT")
