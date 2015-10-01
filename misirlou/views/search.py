7f845ad517c420d4f00a4ba943a6beb027b79481
Alex Parmentier <a.g.parmentier@gmail.com>
2015-10-01 17:54:01 -0400

    WIP: Basic search responses.
    The search view will now return whatever solr gave it.

--- misirlou/views/search.py
+++ misirlou/views/search.py
@@ -5,7 +5,9 @@ from rest_framework import status
 from rest_framework import generics
 
 from misirlou.renderers import SinglePageAppRenderer
-
+from django.conf import settings
+import scorched
+import json
 
 class SearchView(generics.GenericAPIView):
     renderer_classes = (SinglePageAppRenderer, JSONRenderer,
@@ -13,7 +15,13 @@ class SearchView(generics.GenericAPIView):
 
     def get(self, request, *args, **kwargs):
         if request.GET.get('q'):
-            return Response({'q': request.GET.get('q')})
+            solr_conn = scorched.SolrInterface(settings.SOLR_SERVER)
+            response = solr_conn.query(request.GET.get('q')).execute()
+            results = {'results': response.result.docs}
+            import pdb
+            pdb.set_trace()
+            return Response(results,
+                            content_type="charset=utf-8")
         else:
             return Response({'error': 'Did not provide query (q).'},
                             status=status.HTTP_400_BAD_REQUEST)
