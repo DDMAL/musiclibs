4ce1debedc9b52b5cb174131ac39875db05aaecb
Alex Parmentier <a.g.parmentier@gmail.com>
2015-10-01 13:00:43 -0400

    WIP: Switched to class-based manifest instantiation.
    I was writing a ton of interdependent functions that kept passing dicts between each other, and I suddenly remembered what classes are. This should make it much simpler to deal with the multiple sequential steps that need to be taken to validate, index, and create a db entry for manifests.

--- misirlou/tasks.py
+++ misirlou/tasks.py
@@ -1,33 +1,66 @@
 from __future__ import absolute_import
 
 from celery import shared_task
-from celery.app.task import Task
 from celery import current_app
 from celery.signals import after_task_publish
 from .models.manifest import Manifest
-import uuid
 import urllib.request
 import json
 
 @shared_task
 def create_manifest(remote_url, shared_id):
-    #Temporarily using online validator.
-    #TODO port IIIF validator to py3
-    #TODO fix url formatting issues (check for 'http://')
-    v_url = "http://iiif.io/api/presentation/validator/service/validate" \
-            "?format=json&version=2.0&url="
-    v_resp = urllib.request.urlopen(v_url + remote_url)
-    v_data = v_resp.read().decode('utf-8')
-    v_res = json.loads(v_data)
+    """Validate, index, and create a database entry for the manifest
+    :param remote_url: The manifest's url.
+    :param shared_id:  The UUID to be assigned to the manifest.
+    """
 
-    if v_res['error'] != "None":
+    wip_man = WIPManifest(remote_url, shared_id)
+    wip_man.validate_online()
+    if wip_man.errors.get('validation'):
         create_manifest.update_state(state='FAILURE')
-        return {'error': v_res['error'], 'warnings': v_res.get('warnings')}
+        return {'error': wip_man.errors.get('validation')}
 
-    manifest = Manifest(remote_url=remote_url, uuid=uuid.UUID(shared_id))
-    manifest.save()
-    if v_res['warnings'] != "None":
-        return {'warnings': v_res.get('warnings')}
+    wip_man.retrieve_json()
+    wip_man.create_db_entry()
+
+    data = {'status': 'SUCCESS', 'WIPManifest': wip_man}
+    if wip_man.warnings:
+        data['warnings'] = wip_man.warnings
+    return data
+
+class WIPManifest:
+    # A class for manifests that are being built
+    def __init__(self, url, shared_id):
+        self.url = url
+        self.id = shared_id
+        self.json = {}
+        self.errors = {}
+        self.warnings = {}
+
+    def validate_online(self):
+        v_url = "http://iiif.io/api/presentation/validator/service/validate" \
+                "?format=json&version=2.0&url="
+        v_resp = urllib.request.urlopen(v_url + self.url)
+        v_data = v_resp.read().decode('utf-8')
+        v_data = json.loads(v_data)
+
+        if v_data.get('error') != "None":
+            self.errors['validation'] = v_data.get('error')
+
+        if v_data.get('warnings') != "None":
+            self.warnings['validation'] = v_data.get('warnings')
+
+    def retrieve_json(self):
+        manifest_resp = urllib.request.urlopen(self.url)
+        manifest_data = manifest_resp.read().decode('utf-8')
+        self.json = json.loads(manifest_data)
+
+    def solr_index(self):
+        self.retrieve_json()
+
+    def create_db_entry(self):
+        manifest = Manifest(remote_url=self.url, uuid=self.id)
+        manifest.save()
 
 
 @after_task_publish.connect
