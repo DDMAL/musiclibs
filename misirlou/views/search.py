ae3eb4a6321fcbc8c46800ecaaa07780fc65d9d6
Alex Parmentier <a.g.parmentier@gmail.com>
2015-10-02 14:43:53 -0400

    WIP: Reordered to avoid excess indentation.

--- misirlou/views/search.py
+++ misirlou/views/search.py
@@ -7,21 +7,16 @@ from rest_framework import generics
 from misirlou.renderers import SinglePageAppRenderer
 from django.conf import settings
 import scorched
-import json
 
 class SearchView(generics.GenericAPIView):
     renderer_classes = (SinglePageAppRenderer, JSONRenderer,
                         BrowsableAPIRenderer)
 
     def get(self, request, *args, **kwargs):
-        if request.GET.get('q'):
-            solr_conn = scorched.SolrInterface(settings.SOLR_SERVER)
-            response = solr_conn.query(request.GET.get('q')).execute()
-            results = {'results': response.result.docs}
-            import pdb
-            pdb.set_trace()
-            return Response(results,
-                            content_type="charset=utf-8")
-        else:
+        if not request.GET.get('q'):
             return Response({'error': 'Did not provide query (q).'},
                             status=status.HTTP_400_BAD_REQUEST)
+        solr_conn = scorched.SolrInterface(settings.SOLR_SERVER)
+        response = solr_conn.query(request.GET.get('q')).execute()
+        results = {'results': response.result.docs}
+        return Response(results, content_type="charset=utf-8")
