2136f7c3b07388f530852d1c9bed7486a92c8f34
William Bain <bain.william.a@gmail.com>
2015-10-15 17:28:27 -0400

    Refactor: Combine import statements

--- misirlou/views/search.py
+++ misirlou/views/search.py
@@ -1,6 +1,5 @@
 from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer
-from rest_framework.renderers import BrowsableAPIRenderer
+from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 from rest_framework import status
 from rest_framework import generics
 
