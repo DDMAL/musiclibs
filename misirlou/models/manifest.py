f909761fca100037e3fd9159188ddc991a5d284e
Alex Parmentier <a.g.parmentier@gmail.com>
2015-11-06 13:48:21 -0500

    New: Added utility function to re-index everything.

--- misirlou/models/manifest.py
+++ misirlou/models/manifest.py
@@ -19,6 +19,13 @@ class Manifest(models.Model):
         from django.core.urlresolvers import reverse
         return reverse('manifest-detail', args=[str(self.id)])
 
+    def re_index(self):
+        import misirlou.tasks as tasks
+        text_id = str(self.id)
+        tasks.create_manifest.apply_async(args=[self.remote_url, text_id],
+                                          task_id=text_id)
+
+
 @receiver(post_delete, sender=Manifest)
 def solr_delete(sender, instance, **kwarfs):
     import scorched
