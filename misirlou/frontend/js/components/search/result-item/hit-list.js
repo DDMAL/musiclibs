8adf009d662279820cccbacb944e0ebecf73ff83
William Bain <bain.william.a@gmail.com>
2016-01-21 18:56:01 -0500

    New: Handle more hit search fields

--- misirlou/frontend/js/components/search/result-item/hit-list.js
+++ misirlou/frontend/js/components/search/result-item/hit-list.js
@@ -4,12 +4,16 @@ import Im from 'immutable';
 import DescriptionList from '../../ui/description-list';
 
 
+const HIT_WITH_LANG = /(.*)_txt_(.*)/;
+
+
 /** Display a list of hits */
 export default function HitList({ hits })
 {
     const terms = Im.Seq(hits)
-        .filter(hit => hit.field !== 'metadata' && hit.field.indexOf('_') === -1)
+        .filter(hit => hit.field.indexOf('metadata') === -1)
         .map(getTerm)
+        .reduce(getPreferredLang, Im.Map())
         .toArray();
 
     if (terms.length === 0)
@@ -25,15 +29,41 @@ HitList.propTypes = {
 /** Translate a hit into a user-friendly term for display */
 function getTerm(hit)
 {
-    const term = hit.field.charAt(0).toUpperCase() + hit.field.slice(1);
+    let lang = null;
+    let field = hit.field;
+
+    const langCoded = HIT_WITH_LANG.exec(field);
+
+    if (langCoded)
+    {
+        field = langCoded[1];
+        lang = langCoded[2];
+    }
+
+    const term = field.charAt(0).toUpperCase() + field.slice(1);
     const description = hit.parsed.map((text, i) => (
         i % 2 === 0 ? <span key={i}>{text}</span> : <mark key={i}>{text}</mark>
     ));
 
     return {
         term,
+        lang,
         description
     };
+}
+
+function getPreferredLang(prefs, hit)
+{
+    if (hit.lang === 'en' || !prefs.has(hit.term))
+        return prefs.set(hit.term, hit);
+
+    return prefs.update(hit.term, prior =>
+    {
+        if (prior.lang === null || prior.lang === 'en')
+            return prior;
+
+        return hit;
+    });
 }
 
 export const __hotReload = true;
