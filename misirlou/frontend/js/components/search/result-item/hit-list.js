5858c09bb355592cf24f2d982a5e68f68eb30a11
William Bain <bain.william.a@gmail.com>
2016-01-21 14:59:01 -0500

    Refactor: Give map function standalone definition

--- misirlou/frontend/js/components/search/result-item/hit-list.js
+++ misirlou/frontend/js/components/search/result-item/hit-list.js
@@ -3,17 +3,13 @@ import Im from 'immutable';
 
 import DescriptionList from '../../ui/description-list';
 
+
 /** Display a list of hits */
-function HitList({ hits })
+export default function HitList({ hits })
 {
     const terms = Im.Seq(hits)
         .filter(hit => hit.field !== 'metadata' && hit.field.indexOf('_') === -1)
-        .map((hit, i) => ({
-            term: hit.field.charAt(0).toUpperCase() + hit.field.slice(1),
-            description: hit.parsed.map((text, i) => (
-                i % 2 === 0 ? <span key={i}>{text}</span> : <mark key={i}>{text}</mark>
-            ))
-        }))
+        .map(getTerm)
         .toArray();
 
     if (terms.length === 0)
@@ -26,6 +22,18 @@ HitList.propTypes = {
     hits: PropTypes.objectOf(Im.List)
 };
 
-export default HitList;
+/** Translate a hit into a user-friendly term for display */
+function getTerm(hit)
+{
+    const term = hit.field.charAt(0).toUpperCase() + hit.field.slice(1);
+    const description = hit.parsed.map((text, i) => (
+        i % 2 === 0 ? <span key={i}>{text}</span> : <mark key={i}>{text}</mark>
+    ));
+
+    return {
+        term,
+        description
+    };
+}
 
 export const __hotReload = true;
