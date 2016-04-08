ea114faec3ba95fc777dcee57b951345b0fb549e
Alex Parmentier <a.g.parmentier@gmail.com>
2016-04-08 08:59:22 -0400

    Docs: Added celerybeat to startup script.
    The startup script contains a command to start a celery beat. Also am .gitignore'ing files generated by the celery-beat program.

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+# Ctrl+C will kill all the processes started by this script.
 trap 'killall' INT
-
 killall() {
     trap '' INT TERM
     kill -TERM 0
     wait
 }
 
+# Start all the dev servers in async.
 source .env/bin/activate
 python manage.py runserver_plus &
-celery -A misirlou  worker -l info -P eventlet -c 20  &
+celery -A misirlou  worker -l info -P eventlet -c 1000 &
+celery -A misirlou beat &
 cat
