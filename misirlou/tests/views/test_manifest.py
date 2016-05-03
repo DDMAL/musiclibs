3c5cd2cbfe42ca0db1980dff2f651fcb1d373eaf
Alex Parmentier <a.g.parmentier@gmail.com>
2016-05-03 09:45:19 -0400

    WIP: Added tests for the manifest view.
    Added tests to ensure posting to the /manifest endpoint returns a status, and that eventually this posting results in a succesfully imported manifest.

---
+++ misirlou/tests/views/test_manifest.py
@@ -0,0 +1,40 @@
+from misirlou.tests.mis_test import MisirlouTestSetup
+import time
+from misirlou.helpers.IIIFImporter import WIPManifest
+import uuid
+
+
+class ManifestViewTestCase(MisirlouTestSetup):
+
+    def test_post(self):
+        """Test that posting in returns a url to check status of job."""
+        rem_url = "http://localhost:8888/misirlou/tests/manifest.json"
+        resp = self.client.post("/manifests/", {'remote_url': rem_url})
+        self.assertTrue(bool(resp.data['status']))
+
+    def test_post_with_status(self):
+        rem_url = "http://localhost:8888/misirlou/tests/manifest.json"
+        resp = self.client.post("/manifests/", {'remote_url': rem_url})
+        self.assertTrue(bool(resp.data['status']))
+
+        resp2 = self.client.get(resp.data['status'])
+
+        # Wait for the celery import to finish while polling.
+        time_waited = 0
+        while resp2.data['status'] == 1:
+            if time_waited >= 5:
+                raise TimeoutError("Waited over 5 seconds for celery to import.")
+            time.sleep(0.5)
+            time_waited += 0.5
+            resp2 = self.client.get(resp.data['status'])
+
+        expected = {'total_count': 1,
+                    'succeeded_count': 1,
+                    'failed': {},
+                    'failed_count': 0,
+                    'status': 0
+                    }
+        # Assert succeeded key is there, but don't care about resulting url.
+        self.assertTrue(bool(resp2.data['succeeded'][rem_url]))
+        del resp2.data['succeeded']
+        self.assertDictEqual(resp2.data, expected)
