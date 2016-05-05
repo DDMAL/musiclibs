ea15b9c6c226ed57f99adfc3eb39e8f37ec74c57
William Bain <bain.william.a@gmail.com>
2016-05-05 13:34:51 -0400

    Refactor: Simplify template configuration

--- misirlou/renderers/single_page_app_renderer.py
+++ misirlou/renderers/single_page_app_renderer.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from rest_framework.renderers import TemplateHTMLRenderer
-import copy
 
 class SinglePageAppRenderer (TemplateHTMLRenderer):
     """
@@ -12,24 +11,12 @@ class SinglePageAppRenderer (TemplateHTMLRenderer):
 
     def resolve_context(self, data, request, response):
         """
-        Inject the data into the context object under its own key, "view_data"
+        Pass the data into the context object under a key, `view_data`,
+        and pass in the client-side debug configuration.
+        """
+        view_data = super().resolve_context(data, request, response)
 
-        This is similar to what happens in generic Django API views with
-        context_object_name. Django Rest Framework may provide a more ergonomic
-        way to do this in the future.
-
-        See https://github.com/tomchristie/django-rest-framework/issues/1673
-        """
-        context = super().resolve_context(data, request, response)
-        """
-        This was causing infinite recursion in the new restframework version,
-        which appears to be fixed by making a copy of the data dict rather
-        than simply referencing it. I don't have a deep understanding of this
-        issue, so please feel free to correct to a more graceful solution if
-        one exists. -AP
-        """
-        context.update({
-            'view_data': copy.copy(data),
+        return {
+            'view_data': view_data,
             'DEBUG_CLIENT_SIDE': settings.DEBUG_CLIENT_SIDE
-        })
-        return context
+        }
