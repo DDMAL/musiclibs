f8d4a908201ce4106c7f29e16b14454f686ef36d
Alex Parmentier <a.g.parmentier@gmail.com>
2016-05-10 16:45:57 -0400

    Formatting: Fixed questionable line continuations.
    It was pointed out to me that that leading space before the \ looks funny, and one line was only about 83 chars anyway.

--- misirlou/views/main.py
+++ misirlou/views/main.py
@@ -34,8 +34,8 @@ def do_minimal_search(request):
         start = 0
 
     solr_conn = scorched.SolrInterface(settings.SOLR_SERVER)
-    response = solr_conn.query(request.GET.get('q')) \
-        .set_requesthandler('minimal') \
+    response = solr_conn.query(request.GET.get('q'))\
+        .set_requesthandler('minimal')\
         .paginate(start=start).execute()
 
     return format_response(request, response)
