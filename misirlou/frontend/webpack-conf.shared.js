79a93ab867d0e6d8b166bf29a7cda7c2919835bb
William Bain <bain.william.a@gmail.com>
2016-05-13 13:28:47 -0400

    Refactor: Change how Webpack config is specified
    Merge multiple objects together instead of using a bunch of inline
    conditionals.

---
+++ misirlou/frontend/webpack-conf.shared.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/* eslint-env node */
+
+var webpack = require('webpack');
+var mergeWith = require('lodash.mergewith');
+var ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+module.exports = function ()
+{
+    var sharedJQueryPath = require.resolve('jquery');
+    var base = {
+        module: {
+            loaders: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: 'babel'
+                }
+            ]
+        },
+
+        resolve: {
+            alias: {
+                // We need all usages of jQuery to resolve to the same library
+                // not only becuase bundling duplicates would be bad (that could
+                // be optimized later), but because Diva injects its public code
+                // in one of them
+                jquery: sharedJQueryPath
+            }
+        },
+
+        plugins: [
+            new ProgressBarPlugin(),
+
+            // Inject globals that Diva relies on. While this plugin applies
+            // globally, ESLint should ensure that these aren't injected in
+            // app code.
+            //
+            // Note that we need to get Diva to rely on the shared jQuery instance.
+            new webpack.ProvidePlugin({
+                'diva': 'diva',
+                '$': sharedJQueryPath,
+                'jQuery': sharedJQueryPath,
+                'window.jQuery': sharedJQueryPath
+            })
+        ]
+    };
+
+    var configs = [base].concat(Array.prototype.slice.call(arguments));
+
+    return configs.reduce(mergeConf);
+};
+
+function mergeConf(source, other)
+{
+    return mergeWith(source, other, mergeFn);
+}
+
+// Concatenate arrays together
+function mergeFn(source, other)
+{
+    if (Array.isArray(source))
+    {
+        return source.concat(other);
+    }
+}
--- misirlou/frontend/webpack.config.js
+++ misirlou/frontend/webpack.config.js
@@ -4,95 +4,75 @@
 
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
-var sharedJQueryPath = require.resolve('jquery');
+var extendBaseConf = require('./webpack-conf.shared');
 
-module.exports = {
+var pathConf = {
     entry: __dirname + '/js/index.js',
 
     output: {
         filename: 'main.js',
         path: __dirname + '/js/bundle',
         publicPath: '/static/js/bundle/'
-    },
-
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: getCssLoader('style-loader', 'css-loader?-url')
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel'
-            }
-        ]
-    },
-
-    devtool: (process.env.NODE_ENV === 'production') ? 'source-map' : 'inline-source-map',
-
-    resolve: {
-        alias: {
-            // We need all usages of jQuery to resolve to the same library
-            // not only becuase bundling duplicates would be bad (that could
-            // be optimized later), but because Diva injects its public code
-            // in one of them
-            jquery: sharedJQueryPath
-        }
-    },
-
-    plugins: [
-        new ProgressBarPlugin(),
-
-        // Inject globals that Diva relies on. While this plugin applies
-        // globally, ESLint should ensure that these aren't injected in
-        // app code.
-        //
-        // Note that we need to get Diva to rely on the shared jQuery instance.
-        new webpack.ProvidePlugin({
-            'diva': 'diva',
-            '$': sharedJQueryPath,
-            'jQuery': sharedJQueryPath,
-            'window.jQuery': sharedJQueryPath
-        })
-    ].concat(process.env.NODE_ENV === 'production' ? getProductionPlugins() : []),
-
-    devServer: {
-        port: process.env.npm_package_config_dev_server_port || 8001,
-
-        proxy: {
-            '*': 'http://localhost:' + (process.env.npm_package_config_dev_server_proxy_port || 8000)
-        },
-
-        stats: {
-            cached: false
-        }
     }
 };
 
+var envConf;
 
-function getCssLoader(before, loader)
+if (process.env.NODE_ENV === 'production')
 {
-    if (process.env.NODE_ENV === 'production')
-        return ExtractTextPlugin.extract(before, loader);
+    envConf = {
+        devtool: 'source-map',
 
-    return before + '!' + loader;
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract('style-loader', 'css-loader?-url')
+                }
+            ]
+        },
+
+        plugins: [
+            new ExtractTextPlugin('[name].css'),
+            new webpack.DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify('production')
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            }),
+            new webpack.optimize.OccurrenceOrderPlugin(true)
+        ]
+    };
 }
-
-function getProductionPlugins()
+else
 {
-    return [
-        new ExtractTextPlugin('[name].css'),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('production')
-        }),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
+    envConf = {
+        devtool: 'inline-source-map',
+
+        module: {
+            loaders: [
+                {
+                    test: /\.css$/,
+                    loader: 'style-loader!css-loader?-url'
+                }
+            ]
+        },
+
+        devServer: {
+            port: process.env.npm_package_config_dev_server_port || 8001,
+
+            proxy: {
+                '*': 'http://localhost:' + (process.env.npm_package_config_dev_server_proxy_port || 8000)
+            },
+
+            stats: {
+                cached: false
             }
-        }),
-        new webpack.optimize.OccurrenceOrderPlugin(true)
-    ];
+        }
+    };
 }
+
+module.exports = extendBaseConf(pathConf, envConf);
