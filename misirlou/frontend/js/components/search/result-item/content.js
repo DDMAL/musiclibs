26839e72dc24939993ad8a6c0ea4c502a830c636
William Bain <bain.william.a@gmail.com>
2016-05-17 21:52:10 -0400

    New: Add attribution to search results
    And fix some null cases.
    
    Closes #39

--- misirlou/frontend/js/components/search/result-item/content.js
+++ misirlou/frontend/js/components/search/result-item/content.js
@@ -5,6 +5,7 @@ import { manifestSummaryType } from '../../../iiif-types';
 
 import './search-result-item.css';
 import Description from './description';
+import ExternalHtml from '../../external-content/external-html';
 import HitList from './hit-list';
 
 
@@ -13,8 +14,19 @@ export default function Content({ result })
 {
     return (
         <div className="search-result__item__content">
-            <h2 className="h4"><Link to={`/manifests/${result['local_id']}/`}>{result.label}</Link></h2>
-            <Description text={result.description} />
+            <h2 className="h4">
+                <Link to={`/manifests/${result['local_id']}/`}>{result.label}</Link>
+            </h2>
+            {result.description && <Description text={result.description} />}
+            {/* FIXME(wabain): Do margin in CSS */}
+            {result.attribution && (
+                <dl style={{ marginBottom: 10 }}>
+                    <dt>Source</dt>
+                    <dd>
+                        <ExternalHtml>{result.attribution}</ExternalHtml>
+                    </dd>
+                </dl>
+            )}
             <HitList hits={result.hits} />
         </div>
     );
