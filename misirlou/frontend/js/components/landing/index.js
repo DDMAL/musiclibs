3312cd21a61e839fa35c9046cb9ba6228cd18f9e
William Bain <bain.william.a@gmail.com>
2016-05-19 14:44:32 -0400

    New: Hide Recently uploaded listing when searching
    Refs #48

--- misirlou/frontend/js/components/landing/index.js
+++ misirlou/frontend/js/components/landing/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import CSSTransitionGroup from 'react-addons-css-transition-group';
 import { locationShape } from 'react-router';
 
 import SearchContainer from '../search/index';
 import ManifestCascade from './manifest-cascade/index';
+
+import './landing-page.scss';
+
+
+const TRANSITION_SETTINGS = {
+    transitionName: {
+        enter: 'recent-uploads--enter',
+        leave: 'recent-uploads--leave'
+    },
+    transitionEnterTimeout: 400,
+    transitionLeaveTimeout: 250
+};
+
 
 /**
  * Render the landing page, which features a search function and a cascade of
@@ -14,11 +28,16 @@ export default function LandingPage({ location })
         <div className="container">
             <h1 className="text-center">Musiclibs</h1>
             <SearchContainer location={location} />
-
-            <header className="page-header">
-                <h2>Recently uploaded</h2>
-            </header>
-            <ManifestCascade />
+            <CSSTransitionGroup {...TRANSITION_SETTINGS}>
+                {!location.query.q && (
+                    <section key="recent-section">
+                        <header className="page-header">
+                            <h2>Recently uploaded</h2>
+                        </header>
+                        <ManifestCascade />
+                    </section>
+                )}
+            </CSSTransitionGroup>
         </div>
     );
 }
