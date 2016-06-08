c1ed773a5edee34bff2743d79dab33bfbc5c47c6
Alex Parmentier <a.g.parmentier@gmail.com>
2016-06-08 09:13:24 -0400

    Fixed: Updated tests.
    A test had to be removed, and another slighlty updated for the new
    testing we do on manifests.

--- misirlou/tests/tasks/test_tasks.py
+++ misirlou/tests/tasks/test_tasks.py
@@ -1,7 +1,5 @@
 from misirlou.tests.mis_test import MisirlouTestSetup
 from misirlou.tasks import *
-from misirlou.helpers.IIIFImporter import WIPManifest
-import uuid
 import requests
 
 
@@ -22,10 +20,3 @@ class CeleryTaskTestCase(MisirlouTestSetup):
         status, imp_id, url, errors, warnings = imp_result
         expected = (-1, ["not a valid value for dictionary value @ data['@type']"], [])
         self.assertTupleEqual((status, errors, warnings), expected)
-
-    def test_get_document(self):
-        rem_url = "http://localhost:8888/misirlou/tests/fixtures/manifest.json"
-        rem_data = get_document(rem_url)
-        with open("misirlou/tests/fixtures/manifest.json") as f:
-            local_data = f.read()
-        self.assertEqual(rem_data, local_data)
