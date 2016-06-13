57844f1b31548a9463891ddff6bc943b9959a844
Alex Parmentier <a.g.parmentier@gmail.com>
2016-06-13 11:37:58 -0400

    Fixed: Better patching.
    This is less cool, but much nicer and less error prone.

--- misirlou/helpers/schema_validator/library_specific_exceptions.py
+++ misirlou/helpers/schema_validator/library_specific_exceptions.py
@@ -18,34 +18,14 @@ library eventually corrects their manifests, re-importing will not result in
 erroneous corrections.
 """
 from misirlou.helpers.schema_validator.manifest_schema import ManifestSchema
-import contextlib
-
-original = ManifestSchema()
-
 
 def get_harvard_edu():
-
     # Append a context to the image services.
-    def image_service(self, value):
-        val = original.image_service(value)
-        if not val.get('@context'):
-            val['@context'] = 'http://library.stanford.edu/iiif/image-api/1.1/context.json'
-        return val
+    class PatchedManifestSchema(ManifestSchema):
+        def image_service(self, value):
+            val = super().image_service(value)
+            if not val.get('@context'):
+                val['@context'] = 'http://library.stanford.edu/iiif/image-api/1.1/context.json'
+            return val
+    return PatchedManifestSchema()
 
-    with monkey_patch([image_service]):
-        return ManifestSchema()
-
-
-@contextlib.contextmanager
-def monkey_patch(functions):
-    """Patch an iterable of functions, then reset on exit."""
-    originals = {}
-    for fn in functions:
-        fn_name = fn.__name__
-        originals[fn_name] = ManifestSchema.__dict__[fn_name]
-        setattr(ManifestSchema, fn_name, fn)
-    try:
-        yield
-    finally:
-        for fn_name, fn in originals.items():
-            setattr(ManifestSchema, fn_name, fn)
