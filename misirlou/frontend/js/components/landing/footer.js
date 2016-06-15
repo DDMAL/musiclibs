f46a80a01f56299f93174710cdb31b73182a2ce6
jeromepl <jerome-pl2@hotmail.ca>
2016-06-15 10:05:14 -0400

    WIP: Added images and links to footer

--- misirlou/frontend/js/components/landing/footer.js
+++ misirlou/frontend/js/components/landing/footer.js
@@ -7,17 +7,23 @@ export default function Footer()
 {
     return (
         <footer>
-            <div className="container-fluid">
-                <div className="footer--left">
-                    <a href="https://github.com/ddmal/musiclibs"><h6>Github</h6></a>
-                </div>
-                <div className="footer--center">
-                    <h6>About us: We are cool</h6>
-                </div>
-                <div className="footer--right">
-                    <a href="https://simssa.ca"><h6>SIMSSA</h6></a>
-                </div>
-            </div>
+            <table>
+                <tbody>
+                    <tr>
+                        <td className="footer--left">
+                            <a href="https://twitter.com/simssaproject"><img src="/static/Twitter_logo.png"/></a>
+                            <a href="https://github.com/ddmal/musiclibs"><img src="/static/GitHub_logo.png"/></a>
+                        </td>
+                        <td className="footer--right">
+                            <a className="footer__about" href="#"><h5>About</h5></a>
+                            <a className="footer__ddmal-logo" href="http://ddmal.music.mcgill.ca/">
+                                <img src="/static/DDMAL_logo.png"/>
+                            </a>
+                            <a href="https://simssa.ca/"><img src="/static/SIMSSA_logo.png"/></a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </footer>
     );
 }
