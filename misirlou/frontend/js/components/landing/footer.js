59f3da3ed1671dc0c2842501490e02d5149f7632
jeromepl <jerome-pl2@hotmail.ca>
2016-06-15 12:19:47 -0400

    New: Footer links now open in new page

--- misirlou/frontend/js/components/landing/footer.js
+++ misirlou/frontend/js/components/landing/footer.js
@@ -11,15 +11,23 @@ export default function Footer()
                 <tbody>
                     <tr>
                         <td className="footer--left">
-                            <a href="https://twitter.com/simssaproject"><img src="/static/Twitter_logo.png"/></a>
-                            <a href="https://github.com/ddmal/musiclibs"><img src="/static/GitHub_logo.png"/></a>
+                            <a href="https://twitter.com/simssaproject" target="_blank">
+                                <img src="/static/Twitter_logo.png"/>
+                            </a>
+                            <a href="https://github.com/ddmal/musiclibs" target="_blank">
+                                <img src="/static/GitHub_logo.png"/>
+                            </a>
                         </td>
                         <td className="footer--right">
-                            <a className="footer__about" href="#"><h5>About</h5></a>
-                            <a className="footer__ddmal-logo" href="http://ddmal.music.mcgill.ca/">
+                            <a className="footer__about" href="#">
+                                <h5>About</h5>
+                            </a>
+                            <a className="footer__ddmal-logo" href="http://ddmal.music.mcgill.ca/" target="_blank">
                                 <img src="/static/DDMAL_logo.png"/>
                             </a>
-                            <a href="https://simssa.ca/"><img src="/static/SIMSSA_logo.png"/></a>
+                            <a href="https://simssa.ca/" target="_blank">
+                                <img src="/static/SIMSSA_logo.png"/>
+                            </a>
                         </td>
                     </tr>
                 </tbody>
