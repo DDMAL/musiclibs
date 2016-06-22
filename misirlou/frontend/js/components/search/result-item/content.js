07c389d1da68424841a643fa0106652080181681
Alex Parmentier <a.g.parmentier@gmail.com>
2016-06-22 08:24:38 -0400

    New: Added hostname in result + codestyle.
    References #84

--- misirlou/frontend/js/components/search/result-item/content.js
+++ misirlou/frontend/js/components/search/result-item/content.js
@@ -12,23 +12,24 @@ import HitList from './hit-list';
 /** Display basic information for a search result, linking to the full manifest */
 export default function Content({ result, query, pitchQuery })
 {
-    let linkURL = `/manifests/${result["local_id"]}`;
+    let linkURL = `/manifests/${result['local_id']}`;
 
     if (query)
     {
         linkURL += `/?q=${query}`;
-        linkURL += (pitchQuery) ? `&m=${pitchQuery}`: '';
+        linkURL += (pitchQuery) ? `&m=${pitchQuery}` : '';
     }
 
     return (
         <div className="search-result__item__content">
             <h2 className="h4">
-                <Link to={ linkURL }>{result.label}</Link>
+                <Link to={linkURL}>{result.label}</Link> <br/>
+                <small>{getHostname(result['@id'])}</small>
             </h2>
             {result.description && <Description text={result.description} />}
             {/* FIXME(wabain): Do margin in CSS */}
             {result.attribution && (
-                <dl style={{ marginBottom: 10 }}>
+                <dl style={{ marginBottom: 10 }}>q
                     <dt>Source</dt>
                     <dd>
                         <ExternalHtml>{result.attribution}</ExternalHtml>
@@ -42,6 +43,15 @@ export default function Content({ result, query, pitchQuery })
 
 Content.propTypes = {
     result: manifestSummaryType.isRequired,
-    query: PropTypes.string
+    query: PropTypes.string,
+    pitchQuery: PropTypes.string
 };
 
+// Return the host name out from a url.
+function getHostname(url)
+{
+    /* global document */
+    const parser = document.createElement('a');
+    parser.href = url;
+    return parser.hostname;
+}
