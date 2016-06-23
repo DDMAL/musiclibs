575f2adf26d4c6c345d323c1827a04cdb0db0dea
Alex Parmentier <a.g.parmentier@gmail.com>
2016-06-23 14:19:01 -0400

    Fixed: Spelling suggestions work.
    Now the spelling suggestions link will fire a search
    event and produce exactly the kind of behaviour you
    would expect.
    
    Fixes #83

--- misirlou/frontend/js/components/search/spelling-suggestion.js
+++ misirlou/frontend/js/components/search/spelling-suggestion.js
@@ -1,61 +1,83 @@
 import React, { PropTypes } from 'react';
 import Im from 'immutable';
-import { Link } from 'react-router';
+import * as Search from '../../action-creators/search';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+
+const getState = createSelector(
+    ({ search }) => search,
+    (search) => ({ pitchQuery: search.current.pitchQuery })
+);
 
 /** Suggest a correction for the query */
-export default function SpellingSuggestion({ query, spellcheck })
+@connect(getState)
+export default class SpellingSuggestion extends React.Component
 {
-    const correctionText = [];
+    static propTypes = {
+        dispatch: PropTypes.func.isRequired,
+        query: PropTypes.string.isRequired,
+        pitchQuery: PropTypes.string.isRequired,
+        spellcheck: PropTypes.shape({
+            misspellingsAndCorrections: PropTypes.arrayOf(PropTypes.string).isRequired,
+            hits: PropTypes.number.isRequired,
+            collationQuery: PropTypes.string.isRequired
+        }).isRequired
+    };
 
-    // Reconstruct the corrected query instead of using collationQuery because
-    // it inserts backslashes before spaces
-    let newQuery = '';
-    let remaining = query.toLowerCase();
-
-    for (const i of Im.Range(0, spellcheck.misspellingsAndCorrections.length, 2))
+    onClick(event, val)
     {
-        const [original, correction] = spellcheck.misspellingsAndCorrections.slice(i, i + 2);
+        event.preventDefault();
+        this.props.dispatch(Search.request({
+            query: val,
+            pitchQuery: this.props.pitchQuery,
+            suggestions: true }));
+    }
 
-        if (original === correction)
-            continue;
+    render()
+    {
+        const correctionText = [];
 
-        const originalStart = remaining.indexOf(original);
+        // Reconstruct the corrected query instead of using collationQuery because
+        // it inserts backslashes before spaces
+        let newQuery = '';
+        let remaining = this.props.query.toLowerCase();
+        const spellcheck = this.props.spellcheck;
 
-        // If we can't find the original and therefore build the corrected string, bail
-        if (originalStart === -1)
+        for (const i of Im.Range(0, spellcheck.misspellingsAndCorrections.length, 2))
         {
-            console.error(`Failed to find misspelling ${original} in ...${remaining}`);
-            return <noscript />;
+            const [original, correction] = spellcheck.misspellingsAndCorrections.slice(i, i + 2);
+
+            if (original === correction)
+                continue;
+
+            const originalStart = remaining.indexOf(original);
+
+            // If we can't find the original and therefore build the corrected string, bail
+            if (originalStart === -1)
+            {
+                console.error(`Failed to find misspelling ${original} in ...${remaining}`);
+                return <noscript />;
+            }
+
+            const unchangedText = remaining.slice(0, originalStart);
+
+            correctionText.push(unchangedText, <strong key={i}>{correction}</strong>);
+
+            newQuery += unchangedText + correction;
+            remaining = remaining.slice(originalStart + original.length);
         }
 
-        const unchangedText = remaining.slice(0, originalStart);
+        if (remaining)
+        {
+            correctionText.push(remaining);
+            newQuery += remaining;
+        }
 
-        correctionText.push(unchangedText, <strong key={i}>{correction}</strong>);
-
-        newQuery += unchangedText + correction;
-        remaining = remaining.slice(originalStart + original.length);
+        return (
+            <div className="text-muted">
+                Did you mean <a href="#" onClick={event =>
+                this.onClick(event, newQuery)}>{correctionText}</a>?
+            </div>
+        );
     }
-
-    if (remaining)
-    {
-        correctionText.push(remaining);
-        newQuery += remaining;
-    }
-
-    // FIXME: Don't hardcode pathname
-    return (
-        <div className="text-muted">
-            Showing results for <Link to={`/?q=${encodeURIComponent(newQuery)}`}>{correctionText}</Link>
-        </div>
-    );
 }
-
-SpellingSuggestion.propTypes = {
-    query: PropTypes.string.isRequired,
-    spellcheck: PropTypes.shape({
-        misspellingsAndCorrections: PropTypes.arrayOf(PropTypes.string).isRequired,
-        hits: PropTypes.number.isRequired,
-        collationQuery: PropTypes.string.isRequired
-    }).isRequired
-};
-
