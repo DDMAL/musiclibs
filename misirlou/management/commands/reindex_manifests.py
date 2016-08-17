aa3edbe7434d7f00a06f3eabd38d26067a73cac0
Alex Parmentier <a.g.parmentier@gmail.com>
2016-08-17 14:29:44 -0400

    Fixed: reindex management command.
    The command will now do a completely local reindex (that is,
    will retrieve those manifests which are already indexed and
    reindex them). No outside requests are made.
    
    This is useful for re-populating solr after a config change, or
    when new indexing behaviour is introduced.

--- misirlou/management/commands/reindex_manifests.py
+++ misirlou/management/commands/reindex_manifests.py
@@ -1,11 +1,11 @@
+from tqdm import tqdm
 from django.core.management.base import BaseCommand
-
 from misirlou.models.manifest import Manifest
 
 
 class Command (BaseCommand):
-    """Reindex all manifests"""
+    """Reindex all indexed manifests locally."""
 
     def handle(self, *args, **kwargs):
-        for manifest in Manifest.objects.all():
-            manifest.re_index()
+        for manifest in tqdm(Manifest.objects.indexed()):
+            manifest.re_index_from_stored()
