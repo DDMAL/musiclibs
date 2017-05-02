84912b107583843f03feee6e0e3621cf2704c2da
Alex Parmentier <a.g.parmentier@gmail.com>
2017-05-02 14:42:57 -0400

    Fixed: Using celery for reimport command.

--- misirlou/management/commands/reimport_manifests.py
+++ misirlou/management/commands/reimport_manifests.py
@@ -1,9 +1,11 @@
 import time
 
 from django.core.management.base import BaseCommand
+
 from misirlou.models.manifest import Manifest
 from misirlou.helpers.manifest_utils.errors import ErrorMap
 from misirlou.helpers.manifest_utils.importer import ManifestImporter
+from misirlou.tasks import import_single_manifest
 
 class Command (BaseCommand):
     """Reindex all indexed manifests locally."""
@@ -13,7 +15,6 @@ class Command (BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-e', '--error', type=str, default='', help='Code of the error to target.')
         parser.add_argument('-w', '--warning', type=str, default='', help='Code of the warning to target.')
-        parser.add_argument('-d', '--delay', type=float, default=0.3, help="Seconds to wait between requests.")
 
     def handle(self, *args, **options):
         if options['error'] == '' and options['warning'] == '':
@@ -32,7 +33,5 @@ class Command (BaseCommand):
 
         print('Re-importing all manifests with error "{}"'.format(shortmsg))
         for manifest in Manifest.objects.with_error(code):
-            mi = ManifestImporter(manifest.remote_url)
-            mi.create(force=True)
-            time.sleep(options['delay'])
+            import_single_manifest.delay(None, manifest.remote_url)
 
